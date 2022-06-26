4bf5fbe003687019865ef4266d3a6b8d24ee7eb8
Javieroxox <javieroxox@gmail.com>
2022-06-26 04:06:07 -0400

    ya no se cual es
    lo intenten

--- Trabajo/api_jardininfantil/app/Http/Requests/NinosRequest.php
+++ Trabajo/api_jardininfantil/app/Http/Requests/NinosRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use App\Rules\DigitoVerificadorRut;
 
 class NinosRequest extends FormRequest
 {
@@ -25,10 +26,10 @@ class NinosRequest extends FormRequest
     public function rules()
     {
         return[
-            'rut' => 'required|unique:ninos,rut', //ignore($this->niño->rut,'rut'' "esto no funca"
-            'nombre'=>'required|alpha',
-            'apellido'=>'required|alpha',
-            //'id_rango'=>'required|numeric'
+            'rut' => ['bail','required',Rule::unique('ninos')->ignore($this->nino->rut,'rut'),'regex:/^(\d{7,8}-[\dkK])$/',new DigitoVerificadorRut],
+            'nombre'=>'required|alpha|min:3|max:30',
+            'apellido'=>'required|alpha|min:3|max:30',
+            'id_rango'=>'required|numeric'
             ];
     }
 
@@ -36,10 +37,15 @@ class NinosRequest extends FormRequest
     {   return [
         'rut.required' => 'El rut del infante debe ser ingresado',
         'rut.unique'=>'El rut esta duplicado',
+        'rut.regex' => 'Indique RUT sin puntos, con guión y con dígito verificador',
         'nombre.required'=>'El nombre del infante debe ser ingresado',
         'nombre.alpha'=>'Ingreso números en el nombre del infante',
+        'nombre.min' => 'El nombre debe tener como mínimo 3 letras',
+        'nombre.max' => 'El nombre debe tener como máximo 30 letras',
         'apellido.required'=>'El apellido del infante debe ser ingresado',
         'apellido.alpha'=>'Ingreso números en el apellido del infante',
+        'apellido.min' => 'El apellido debe tener como mínimo 3 letras',
+        'apellido.max' => 'El apellido debe tener como máximo 30 letras',
         //'id_rango.required'=>'El rango escolar del infante debe ser ingresado',
         //'id_rango.numeric'=>'Ingreso una letra por error'
         ];
--- Trabajo/api_jardininfantil/database/seeders/DatabaseSeeder.php
+++ Trabajo/api_jardininfantil/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\{Schema,DB};
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +16,66 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        
+
+        // Borrar tablas
+        $tablas = ['rangos','ninos','educadoras'];
+        Schema::disableForeignKeyConstraints();
+        foreach($tablas as $tabla){
+            DB::table($tabla)->truncate();
+        }
+        Schema::enableForeignKeyConstraints();
+
+        //rangos
+        $rangos = [
+            ['nombre'=>'Sala Cuna Menor'],
+            ['nombre'=>'Sala Cuna Mayor'],
+            ['nombre'=>'Nivel Medio Menor'],
+            ['nombre'=>'Nivel Medio Mayor'],
+            ['nombre'=>'Primer  Nivel de Transición'],
+            ['nombre'=>'Segundo Nivel de Transición'],
+        ];
+
+        foreach($rangos as $rango){
+            DB::table('rangos')->insert([
+                'nombre' => $rango['nombre'],
+            ]);
+        }
+
+        //ninos
+        $ninos = [
+            ['rut'=>'19612159-5','nombre'=>'Pedro','apellido'=>'Reyes','id_rango'=>'4'],
+            ['rut'=>'6528305-0','nombre'=>'Claudio','apellido'=>'Rojas','id_rango'=>'1'],
+            ['rut'=>'22906905-5','nombre'=>'Felipe','apellido'=>'Carmen','id_rango'=>'3'],
+            ['rut'=>'24997935-k','nombre'=>'Carlos','apellido'=>'Pulgar','id_rango'=>'6'],
+        ];
+
+        foreach($ninos as $nino){
+            DB::table('ninos')->insert([
+                'rut' => $nino['rut'],
+                'nombre' => $nino['nombre'],
+                'apellido' => $nino['apellido'],
+                'id_rango' => $nino['id_rango'],
+            ]);
+        }
+
+        //educadoras
+        $educadoras = [
+            ['rut'=>'7980740-0','nombre'=>'Victoria','apellido'=>'Torres','id_rango'=>'2'],
+            ['rut'=>'14786476-0','nombre'=>'Rosa','apellido'=>'Maturana','id_rango'=>'5'],
+            ['rut'=>'19517163-7','nombre'=>'Roberta','apellido'=>'Anaty','id_rango'=>'1'],
+            ['rut'=>'17103132-k','nombre'=>'Ely','apellido'=>'Martines','id_rango'=>'2'],
+        ];
+
+        foreach($educadoras as $educadora){
+            DB::table('educadoras')->insert([
+                'rut' => $educadora['rut'],
+                'nombre' => $educadora['nombre'],
+                'apellido' => $educadora['apellido'],
+                'id_rango' => $educadora['id_rango'],
+            ]);
+        }
+
+        //emergencias
+
     }
 }
