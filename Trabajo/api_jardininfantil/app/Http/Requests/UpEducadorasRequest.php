73e02fffe77b4df804f5b037503f1210f4ff9b88
Javieroxox <javieroxox@gmail.com>
2022-06-27 03:15:53 -0400

    aahora son 2
    seed

--- Trabajo/api_jardininfantil/app/Http/Requests/UpEducadorasRequest.php
+++ Trabajo/api_jardininfantil/app/Http/Requests/UpEducadorasRequest.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Http\FormRequest;
 use App\Rules\DigitoVerificadorRut;
 use Illuminate\Validation\Rule;
 
-class EducadorasRequest extends FormRequest
+class UpEducadorasRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
