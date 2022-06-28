530248090ef932674e9b1d44910eba24dda908eb
Bazokhala <lmartin3099@gmail.com>
2022-06-28 17:46:54 -0400

    Intento de hacer get

--- Trabajo/cliente_jardininfantil/lib/pages/ninos_page.dart
+++ Trabajo/cliente_jardininfantil/lib/pages/ninos_page.dart
@@ -1,4 +1,8 @@
+import 'package:cliente_jardininfantil/pages/providers/ninos_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+
+
 
 class NinosPage extends StatefulWidget {
   NinosPage({Key? key}) : super(key: key);
@@ -13,6 +17,46 @@ class _NinosPageState extends State<NinosPage> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Niños Jardin'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(5.0),
+        child: Column(
+          children: [
+            Expanded(
+              child: FutureBuilder(
+                future: NinosProvider().getNinos(),
+                builder: (context, AsyncSnapshot snap){
+                  if(!snap.hasData){
+                    return Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }return ListView.separated(
+                    separatorBuilder: (_, __) => Divider(),
+                    itemCount: snap.data.length,
+                    itemBuilder: (context, index){
+                      var ninos = snap.data[index];
+                      return Row(
+                        children: [
+                          ListTile(
+                            leading: Icon(MdiIcons.peanut),
+                            title: Text('[${ninos['rut']}] ${ninos['nombre']}'),
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                  // }return Slidable(
+                  //       child: ListTile(
+                  //         leading: Icon(MdiIcons.cube),
+                  //         title: Text('[${prod['cod_producto']}] ${prod['nombre']}'),
+                  //         subtitle: Text('Stock: ${prod['stock']}'),
+                  //         trailing: Text('\$${fPrecio.format(prod['precio'])}'),
+                  //       ),
+                },
+              ),
+            ),
+          ],
+        )  
       ),
     );
   }
--- Trabajo/cliente_jardininfantil/lib/pages/providers/ninos_provider.dart
+++ Trabajo/cliente_jardininfantil/lib/pages/providers/ninos_provider.dart
@@ -3,12 +3,12 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 
-class ProductosProvider {
+class NinosProvider {
   final String apiURL = 'http://10.0.2.2:8000/api';
 
-  //retorna lista de productos
-  Future<List<dynamic>> getProductos() async {
-    var uri = Uri.parse('$apiURL/productos');
+  
+  Future<List<dynamic>> getNinos() async {
+    var uri = Uri.parse('$apiURL/jardin');
     var respuesta = await http.get(uri);
 
     if (respuesta.statusCode == 200) {
